8a8e817ddf9fdda91bf9b1961484b4be9bb167ba
SW-Skku <ki011127@naver.com>
2023-07-07 14:32:50 +0900

    update sql

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-//const initPassport = require('./config/initPassport');
-//initPassport(app);
+const initPassport = require('./config/initPassport');
+initPassport(app);
 
 //import routers
 const workout = require('./routes/workout_routes');
@@ -12,7 +12,6 @@ const account = require('./routes/account_routes');
 const set = require('./routes/set_routes');
 const motion = require('./routes/motion_routes');
 const routine = require('./routes/routine_routes');
-const packet = require('./routes/packet_routes');
 
 //router settings
 app.use('/workout', workout);
@@ -21,7 +20,6 @@ app.use('/set', set);
 app.use('/account', account);
 app.use('/motion', motion);
 app.use('/routine', routine);
-app.use('/packet', packet);
 
 const port = 4000;
 app.listen(port, () => {
