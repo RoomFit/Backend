f4f8c04bbf3337ed38388c554c703537b4414d75
W00SHH <jerry7780@g.skku.edu>
2023-07-13 15:50:27 +0900

    feature : get feed 추가

---
+++ test/community_test/post_feed.js
@@ -0,0 +1,23 @@
+const data = {
+  user_id: 'user1',
+  feed_content: 'asdfaszzzz',
+  imageUrl:
+    'https://mblogthumb-phinf.pstatic.net/MjAyMTAxMTNfMjQy/MDAxNjEwNTMxNzU0NDAw.dnT66RTVTyv0DURLa16orDdxHfYGkWw2fIf-VvkWZl0g.DLOrmXxdYsTEU_PaIgDXLBgvs9W4lWrvcA78aObeo0Mg.JPEG.sb02199/3.jpg?type=w800',
+};
+
+console.log(JSON.stringify(data));
+
+fetch('http://localhost:4000/community/post-feed', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+  .then(response => response.json())
+  .then(responseData => {
+    console.log('Response:', responseData);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
