a9b07fe06f2c2075c679a612f9566efae3e98faa
SW-Skku <ki011127@naver.com>
2023-07-18 09:19:14 +0900

    modify volume

--- models/workout_model.js
+++ models/workout_model.js
@@ -235,7 +235,7 @@ Workout.stat = (user_id, period, callback) => {
     leg: 0,
     etc: 0,
   };
-  db.all(weight_percentage_query, [], (err, rows) => {
+  db.all(weight_percentage_query, [user_id], (err, rows) => {
     if (err) {
       console.error(err);
       return;
