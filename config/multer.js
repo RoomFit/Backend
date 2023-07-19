8ea5071c0e232a5beef327840b6fea2a10578505
W00SHH <jerry7780@g.skku.edu>
2023-07-19 15:18:56 +0900

    fix : feed post 변경

--- config/multer.js
+++ config/multer.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
+
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -8,6 +9,7 @@ aws.config.update({
 });
 
 const s3 = new aws.S3();
+
 const upload = multer({
   storage: multerS3({
     s3: s3,
--- routes/community_routes.js
+++ routes/community_routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../config/multer');
+
 
 const community_controller = require('../controllers/community_controller');
 
 router.get('/', community_controller.get_feed);
-router.post('/post-feed', community_controller.post_feed);
+router.post('/post-feed', upload.single('image'),community_controller.post_feed);
 
 module.exports = router;
