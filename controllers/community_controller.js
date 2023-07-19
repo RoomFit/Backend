83eb026713ae943cdaf30e3db400ab563601fd9e
W00SHH <jerry7780@g.skku.edu>
2023-07-19 13:19:49 +0900

    Feature: add get posted image

--- controllers/community_controller.js
+++ controllers/community_controller.js
@@ -1,29 +1,32 @@
 const Feed = require('../models/community_model');
 
 const post_feed = (req, res) => {
-  const current_date = new Date().toLocaleString();
-  const new_feed = new Feed({
-    user_id: req.body.user_id,
-    feed_content: req.body.feed_content,
-    imageUrl: req.body.imageUrl,
-    created_at: current_date,
-    updated_at: current_date,
-    like_count: 0,
-  });
+  console.log(req);
+  console.log('res');
+  console.log(res);
+  // const current_date = new Date().toLocaleString();
+  // const new_feed = new Feed({
+  //   user_id: req.body.user_id,
+  //   feed_content: req.body.feed_content,
+  //   imageUrl: req.body.imageUrl,
+  //   created_at: current_date,
+  //   updated_at: current_date,
+  //   like_count: 0,
+  // });
 
-  Feed.create(new_feed, (err, id) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || 'Some error occurred while creating new feed.',
-        success: 0,
-      });
-    else {
-      res.json({
-        feed_id: id,
-        success: 1,
-      });
-    }
-  });
+  // Feed.create(new_feed, (err, id) => {
+  //   if (err)
+  //     res.status(500).send({
+  //       message: err.message || 'Some error occurred while creating new feed.',
+  //       success: 0,
+  //     });
+  //   else {
+  //     res.json({
+  //       feed_id: id,
+  //       success: 1,
+  //     });
+  //   }
+  // });
 };
 
 const get_feed = (req, res) => {
