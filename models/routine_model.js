c0b1f795650977ad3740bd5e44ebdce308aa6184
SW-Skku <ki011127@naver.com>
2023-07-24 15:24:25 +0900

    routine save

--- models/routine_model.js
+++ models/routine_model.js
@@ -23,7 +23,7 @@ Routine.load = function (user_id, limit = false, callback) {
     } else {
       const routineIds = rows.map(row => row.routine_id);
       const placeholders = routineIds.map(() => '?').join(',');
-      const sqlRoutine = `SELECT routine_name, routine.routine_id, motion.major_target FROM routine_motion INNER JOIN routine ON routine.routine_id = routine_motion.routine_id INNER JOIN motion ON motion.motion_id = routine_motion.motion_id WHERE routine_motion.routine_id IN (${placeholders})`;
+      const sqlRoutine = `SELECT routine_name, routine.routine_id, motion.body_region FROM routine_motion INNER JOIN routine ON routine.routine_id = routine_motion.routine_id INNER JOIN motion ON motion.motion_id = routine_motion.motion_id WHERE routine_motion.routine_id IN (${placeholders})`;
       db.all(sqlRoutine, routineIds, (err, routineRows) => {
         //console.log(routineRows);
         if (err) {
@@ -31,30 +31,30 @@ Routine.load = function (user_id, limit = false, callback) {
         } else {
           const groupedResults = {};
           routineRows.forEach(row => {
-            const {routine_id, routine_name, major_target} = row;
+            const {routine_id, routine_name, body_region} = row;
             if (!groupedResults[routine_id]) {
               groupedResults[routine_id] = {
                 routine_id: routine_id,
                 routine_name: routine_name,
-                major_targets: [major_target],
+                body_regions: [body_region],
                 motion_count: 1,
               };
             } else {
               let k = 0;
-              groupedResults[routine_id].major_targets.forEach(target => {
-                if (target.includes(major_target)) {
+              groupedResults[routine_id].body_regions.forEach(target => {
+                if (target.includes(body_region)) {
                   k = 1;
                 }
               });
               if (k == 0) {
-                groupedResults[routine_id].major_targets.push(major_target);
+                groupedResults[routine_id].body_regions.push(body_region);
               }
               groupedResults[routine_id].motion_count++;
             }
           });
           const finalResults = Object.values(groupedResults).map(result => {
             const uniqueMajorTarget = new Set();
-            result.major_targets.forEach(targets => {
+            result.body_regions.forEach(targets => {
               const target = targets.split(',').map(item => item.trim());
               target.forEach(tar => {
                 if (!uniqueMajorTarget.has(tar)) {
@@ -65,7 +65,7 @@ Routine.load = function (user_id, limit = false, callback) {
 
             return {
               ...result,
-              major_targets: [...uniqueMajorTarget].join(', '),
+              body_regions: [...uniqueMajorTarget].join(', '),
             };
           });
           finalResults.sort((a, b) => b.routine_id - a.routine_id);
@@ -76,8 +76,10 @@ Routine.load = function (user_id, limit = false, callback) {
   });
 };
 
+
 Routine.detail = function (routine_id, callback) {
   const sql = `SELECT
+                routine.user_id,
                 routine.routine_id,
                 routine.routine_name,
                 routine_motion.motion_id,
@@ -94,7 +96,7 @@ Routine.detail = function (routine_id, callback) {
     if (err) {
       console.error(err.message);
       callback(err, null);
-    } else if(rows.length > 0){
+    } else if (rows.length > 0) {
       const datas = {
         routine_id: rows[0].routine_id,
         routine_name: rows[0].routine_name,
@@ -103,7 +105,7 @@ Routine.detail = function (routine_id, callback) {
       let rowCount = 0;
       rows.forEach(row => {
         const motionSql =
-          'SELECT motion_id, motion_name, imageUrl FROM motion WHERE motion_id = ?';
+          'SELECT motion_id, motion_name, image_url FROM motion WHERE motion_id = ?';
         const {routine_motion_id, set_id, set_no, weight, rep, mode} = row;
         db.all(motionSql, row.motion_id, (err, motionRows) => {
           if (err) {
@@ -118,8 +120,10 @@ Routine.detail = function (routine_id, callback) {
                 routine_motion_id: routine_motion_id,
                 motion_id: motionRows[0].motion_id,
                 motion_name: motionRows[0].motion_name,
-                imageUrl: motionRows[0].imageUrl,
+                image_url: motionRows[0].image_url,
                 sets: [],
+                motion_range_min: -1,
+                motion_range_max: -1,
               });
             }
 
@@ -132,9 +136,30 @@ Routine.detail = function (routine_id, callback) {
                 reps: rep,
                 mode: mode,
               });
+            rowCount++;
+            if (rowCount === rows.length){
+              let count = 0;
+              datas.motionList.forEach(list => {
+                const sqlMotionRange = `SELECT motion_range_min, motion_range_max FROM motion_range WHERE motion_id = ? AND user_id = ?`;
+                db.get(sqlMotionRange, [list.motion_id, row.user_id], (err, motion_range) => {
+                  if (err) {
+                    console.error(err);
+                  } else {
+                    console.log(motion_range);
+                    list.motion_range_min = motion_range?motion_range.motion_range_min:-1;
+                    list.motion_range_max = motion_range?motion_range.motion_range_max:-1;
+                    count++;
+                    console.log(datas.motionList.length);
+                    console.log(count);
+                    if(count==datas.motionList.length){
+                      console.log(datas);
+                      callback(null,datas);
+                    }
+                  }
+                })
+              });
+            }
           }
-          rowCount++;
-          if (rowCount === rows.length) callback(null, datas);
         });
       });
     }
@@ -150,7 +175,28 @@ Routine.delete = function (routine_ids, callback) {
   });
 };
 
-Routine.save = function (routine_id, motion_list, callback) {
+Routine.save = function (user_id,routine_id, motion_list, callback) {
+  for(let i = 0; i<motion_list.length; i++){
+    const checkData = `SELECT * FROM motion_range WHERE user_id = ? AND motion_id = ?`;
+    db.get(checkData, [user_id,motion_list[i].motion_id], (err, row)=>{
+      if(row){
+        const sqlUpdate = `UPDATE motion_range SET motion_range_min = ?, motion_range_max = ? WHERE motion_id = ? AND user_id = ?`;
+        db.run(sqlUpdate, [motion_list[i].motion_range_min, motion_list[i].motion_range_max, motion_list[i].motion_id, user_id], (err)=>{
+          if(err){
+            console.error(err);
+          }
+        })
+      }
+      else{
+        const sqlInsert = `INSERT INTO motion_range (user_id, motion_id, motion_range_min, motion_range_max) VALUES (?,?,?,?)`;
+        db.run(sqlInsert,[user_id,motion_list[i].motion_id,motion_list[i].motion_range_min,motion_list[i].motion_range_max],(err)=>{
+          if(err){
+            console.error(err);
+          }
+        })
+      }
+    })
+  }
   const sql = `DELETE FROM routine_motion where routine_id = ?`;
   db.run(sql, routine_id, (err, result) => {
     if (err) {
@@ -189,7 +235,55 @@ Routine.save = function (routine_id, motion_list, callback) {
           },
         );
       }
-      callback(null, result);
+      const sqlRoutine = `SELECT routine_name, routine.routine_id, motion.body_region FROM routine_motion INNER JOIN routine ON routine.routine_id = routine_motion.routine_id INNER JOIN motion ON motion.motion_id = routine_motion.motion_id WHERE routine_motion.routine_id = ?`;
+      db.all(sqlRoutine, routine_id, (err, routineRows) => {
+        //console.log(routineRows);
+        if (err) {
+          console.error(err);
+        } else {
+          const groupedResults = {};
+          routineRows.forEach(row => {
+            const {routine_id, routine_name, body_region} = row;
+            if (!groupedResults[routine_id]) {
+              groupedResults[routine_id] = {
+                routine_id: routine_id,
+                routine_name: routine_name,
+                body_regions: [body_region],
+                motion_count: 1,
+              };
+            } else {
+              let k = 0;
+              groupedResults[routine_id].body_regions.forEach(target => {
+                if (target.includes(body_region)) {
+                  k = 1;
+                }
+              });
+              if (k == 0) {
+                groupedResults[routine_id].body_regions.push(body_region);
+              }
+              groupedResults[routine_id].motion_count++;
+            }
+          });
+          const finalResults = Object.values(groupedResults).map(result => {
+            const uniqueMajorTarget = new Set();
+            result.body_regions.forEach(targets => {
+              const target = targets.split(',').map(item => item.trim());
+              target.forEach(tar => {
+                if (!uniqueMajorTarget.has(tar)) {
+                  uniqueMajorTarget.add(tar);
+                }
+              });
+            });
+
+            return {
+              ...result,
+              body_regions: [...uniqueMajorTarget].join(', '),
+            };
+          });
+          finalResults.sort((a, b) => b.routine_id - a.routine_id);
+          callback(null, finalResults);
+        }
+      });
     }
   });
 };
