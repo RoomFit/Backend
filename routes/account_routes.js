5a43fa0f67b45507ca667aeb842b0f11563ef60f
ki011127 <ki011127@g.skku.edu>
2023-09-18 13:50:42 +0900

    회원탈퇴 api

--- routes/account_routes.js
+++ routes/account_routes.js
@@ -42,6 +42,6 @@ router.post('/email-verification', account_controller.email_verification);
 router.put('/change_password', account_controller.change_password);
 router.get('/user-info', account_controller.user_info);
 router.post('/email-duplication', account_controller.check_email);
-
+router.post('/withdraw', account_controller.withdraw);
 
 module.exports = router;
