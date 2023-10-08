fa826a793ef8833318f7bd279295377158b48ced
ki011127 <ki011127@naver.com>
2023-10-09 03:49:35 +0900

    .

--- models/account_model.js
+++ models/account_model.js
@@ -601,7 +601,7 @@ Account.load_range_percents = (user_id, callback) => {
         callback(err);
         return;
       }
-      callback(row);
+      callback(null, row);
     }
   )
 }
@@ -617,7 +617,7 @@ Account.load_measure_counts = (user_id, callback) => {
         callback(err);
         return;
       }
-      callback(row);
+      callback(null, row);
     }
   )
 }
